59437c460cb0d5036c6dc376c19c0abb49a58441
Rehoboth Yimer <xaragawyimer@stuba.sk>
2023-03-05 17:26:47 +0100

    First commit - Test

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -1,7 +1,21 @@
 package sk.stuba.fei.uim.oop;
 
+import static java.util.Arrays.sort;
+import static sk.stuba.fei.uim.oop.Sort.printArray;
+
 public class Main {
     public static void main(String[] args) {
+        int[] arr = { 10, 7, 8, 9, 1, 5 };
+        int n = arr.length;
+        org.example.Test robi = new org.example.Test();
+        robi.setName("robi");
+        robi.setAge(5);
+
+        System.out.println("Hello its me " + robi.getName());
+        System.out.println("my age is  " + robi.getAge());
+        sort(arr, 0, n - 1);
+        System.out.println("Sorted array: ");
+        printArray(arr, n);
 
     }
 }
---
+++ src/main/java/sk/stuba/fei/uim/oop/Test.java
@@ -0,0 +1,20 @@
+package org.example;
+
+public class Test {
+    private  String name;
+    private int age;
+    private int ID;
+    private String surName;
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setAge(int age){
+        this.age = age;
+    }
+    public int getAge(){
+        return age;
+    }
+}
